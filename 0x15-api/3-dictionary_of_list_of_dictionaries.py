48d3e4c0b783e8dfc53c3aec51117fb456e05dc3
root <root@User-PC.localdomain>
2022-10-19 21:42:56 +0300

    fresh and clean

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""Exports data in the JSON format"""
+'''
+This program write user data
+to json file
+'''
 
-if __name__ == "__main__":
+import json
+import requests
+import sys
 
-    import json
-    import requests
-    import sys
+if __name__ == '__main__':
+    usr_url = "https://jsonplaceholder.typicode.com/users"
+    emps_data = requests.get(usr_url).json()
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    filename = "todo_all_employees.json"
+    tasks_obj = []
+    all_tasks = {}
+    _min = 0
+    _max = 20
 
-    users = requests.get("https://jsonplaceholder.typicode.com/users")
-    users = users.json()
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos = todos.json()
-    todoAll = {}
+    with open(filename, mode='w') as f:
+        for emp_data in emps_data:
+            for i in range(_min, _max):
+                task = tasks[i]
+                task_obj = {"task": task.get('title'),
+                            "completed": task.get('completed'),
+                            "username": emp_data.get('username')}
+                tasks_obj.append(task_obj)
 
-    for user in users:
-        taskList = []
-        for task in todos:
-            if task.get('userId') == user.get('id'):
-                taskDict = {"username": user.get('username'),
-                            "task": task.get('title'),
-                            "completed": task.get('completed')}
-                taskList.append(taskDict)
-        todoAll[user.get('id')] = taskList
+            all_tasks[str(emp_data.get('id'))] = tasks_obj
+            tasks_obj = []
+            _min = _max
+            _max = _max + 20
 
-    with open('todo_all_employees.json', mode='w') as f:
-        json.dump(todoAll, f)
+        json.dump(all_tasks, f)

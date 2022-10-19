453896e463a45b6034bbaf5f45345a23acba3d77
samueltezera28 <samueltezera28@gmail.com>
2022-10-19 21:09:53 +0300

    clean

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,14 +16,13 @@ if __name__ == '__main__':
 
     tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
     tasks_compl = 0
-    task_total = 20
     tasks_compl_title = []
     for task in tasks:
         if task.get('userId') == emp_id:
             if task.get('completed'):
                 tasks_compl += 1
                 tasks_compl_title.append(task.get('title'))
-    msg = "Employee {:s} is done with tasks({:d}/{:d}):"
-    print(msg.format(emp_name, tasks_compl, task_total))
+    msg = "Employee {:s} is done with tasks({:d}/20):"
+    print(msg.format(emp_name, tasks_compl))
     for title in tasks_compl_title:
         print('\t {:s}'.format(title))
